55023f9baba16d21bc00650ecf64bbd7bff8acba
samuel <samuel.mbula@student.moringaschool.com>
2023-08-28 12:23:37 +0300

    python-p3 putting-it-all-together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, author, publication_year):
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+
+    def display_info(self):
+        return f"Title: {self.title}, Author: {self.author}, Year: {self.publication_year}"
+
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, color, size):
+        self.brand = brand
+        self.color = color
+        self.size = size
+
+    def is_available_in_size(self, target_size):
+        return self.size == target_size
